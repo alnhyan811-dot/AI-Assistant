4ac5e8c904007aa8c6bf964fdb2fdc246c263f98
alnhyan811-dot <alnhyan811@gmail.com>
2026-06-07 16:50:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import os
 from flask import Flask, render_template
 
-# نحدد للسيرفر أن المجلد الحالي هو مكان العمل
+# نحدد للسيرفر أن مجلد القوالب هو templates
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
-    try:
-        return render_template('index.html')
-    except Exception as e:
-        return str(e) # هذا سيظهر لنا الخطأ بالتفصيل على الشاشة إذا فشل مرة أخرى
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
